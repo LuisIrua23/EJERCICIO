7a848626dfc312bb259b2395b7fef42017319970
Astra <Astra@LUIS-IRUA>
2023-11-12 21:54:13 -0500

    EJERCICIO

--- src/ejercicio/EJERCICIO.java
+++ src/ejercicio/EJERCICIO.java
@@ -27,7 +27,7 @@ public class EJERCICIO {
                         
                         
                         
-                        IORFFDKFKDJK
+                        IORFFDKFKDJKDLMFDFDMKFMDFMKDMFDMFMDLFMLDMFLMDLFMD
     }
     
 }
